539c0555d6c02bd0de93998c966f924697ce4644
Jaime Linares <jlinares@technisys.com>
2022-09-29 20:26:46 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
+
+
+  const renderTodos = (todo) => <div>{todo.name}</div>;
+  const resultRenderTodos = todos.map(renderTodos)
+
+  const handleInput = (event)=> setText(event.target.value)
+
+  const handleOnCick = ()=>setTodos([...todos,text])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <div>
+        <input onChange={handleInput} value={text}></input>
+        <button onClick={handleOnCick}>Add</button>
+      </div>
+      <div>{resultRenderTodos}</div>
+    </main>
   );
 }
 
