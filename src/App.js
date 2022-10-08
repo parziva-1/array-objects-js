bc9e7a2c73d85abf88b60cb6bf62280dc926697e
Jaime Linares <jlinares@technisys.com>
2022-10-07 19:16:21 -0500

    Added localStorage and description

--- src/App.js
+++ src/App.js
@@ -1,49 +1,79 @@
 import { useState } from "react";
 import "./App.css";
 
+import useLocalStorage from "./lib/hooks/useLocalStorage";
+
 //{
 //  name:"correr",
 //   id:0,
 //   status:true
+//   description:""
 //}
 
 function App() {
-  const [todos, setTodos] = useState([{ name: "correr", id: 0, status: true }]);
-  const [text, setText] = useState("");
+  const [todos, setTodos] = useLocalStorage("todos", [
+    { name: "caminar", id: 200, descripcion: "a las 4 pm", status: false },
+  ]);
+
+  const [name, setName] = useState("");
+  const [descripcion, setDescription] = useState("");
   const [id, setId] = useState(1);
+
+  const [error, setError] = useState(false);
 
   const handleDeleTodo = (todoToDelete) =>
     setTodos(todos.filter(({ id }) => id !== todoToDelete));
-    
-  const renderTodos = ({ name, id, status }) => {
+
+  const renderTodos = ({ name, id, status, descripcion }) => {
     console.log(id, status);
     return (
       <div
         style={{ display: "flex", justifyContent: "space-between" }}
         key={id}
       >
-        {name}
-        <input type="checkbox" value={status} />
+        <div>
+          <p>name: {name}</p>
+          <p>descripcion: {descripcion}</p>
+        </div>
+        <input type="checkbox" checked={status} />
         <button onClick={() => handleDeleTodo(id)}>X</button>
       </div>
     );
   };
 
   const resultRenderTodos = todos.map(renderTodos);
-  const handleInput = (event) => setText(event.target.value);
+  const handleInput = (event) => {
+    setError(false);
+    setName(event.target.value);
+  };
+
+  const handleInputDescription = (event) => {
+    setDescription(event.target.value)
+  };
 
   const handleOnCick = () => {
-    setTodos([...todos, { name: text, status: true, id: id }]);
+    if (name === "") {
+      setError(true);
+      return;
+    }
+    setTodos([
+      ...todos,
+      { name: name, status: false, id: id, descripcion: descripcion },
+    ]);
     setId(id + 1);
-    setText("");
+    setName("");
   };
 
   const handleDeleteAllTodos = () => setTodos([]);
 
   return (
     <main className="App">
-      <div>
-        <input onChange={handleInput} value={text}></input>
+      {error ? <p>no puedes agregar una tarea sin un nombre.</p> : ""}
+      <div style={{display:"flex"}}>
+        <div style={{display:"flex", flexDirection:"column"}}>
+          <input onChange={handleInput} value={name} placeholder="name"></input>
+          {name?<textarea onChange={handleInputDescription} value={descripcion} placeholder="description"></textarea>:""}
+        </div>
         <button onClick={handleOnCick}>Add</button>
         <button onClick={handleDeleteAllTodos}>Delete All</button>
       </div>
